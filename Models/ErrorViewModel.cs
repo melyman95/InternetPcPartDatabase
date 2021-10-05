20587358ffad5f9da000defc2e8dffdc7c08bcad
melyman95 <mattlyman12@gmail.com>
2021-10-05 16:43:46 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InternetPcPartDatabase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
