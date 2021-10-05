57d0756a36a3dbd129ae685e3202dffbddedcfd8
melyman95 <mattlyman12@gmail.com>
2021-10-05 16:54:19 -0700

    Made initial part class

---
+++ Models/Part.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternetPcPartDatabase.Models
+{
+    public class Part
+    {
+        [Key]
+        public int PartId {  get; set; }
+
+        public string PartType {  get; set; }
+
+        public string PartName {  get; set; }   
+
+        public string Manufacturer {  get; set; }
+
+        public string MSRP {  get; set; }
+
+        public string ReleaseYear {  get; set; }
+    }
+}
