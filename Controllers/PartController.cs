7032a14d726dd5ec7e3e333f3651aa8a16cc67ba
melyman95 <mattlyman12@gmail.com>
2021-10-06 16:14:45 -0700

    Displays parts from database, need to implement edit, add and remove functions for registered users only

---
+++ Controllers/PartController.cs
@@ -0,0 +1,26 @@
+﻿using InternetPcPartDatabase.Data;
+using InternetPcPartDatabase.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternetPcPartDatabase.Controllers
+{
+    public class PartController : Controller
+    {
+        private readonly PartContext? _context;
+        public PartController(PartContext context)
+        {
+            this._context = context;
+        }
+        // Get parts from database
+        public IActionResult Index()
+        {
+            List<Part> parts = 
+                (from p in _context.Parts
+                select p).ToList();
+
+            return View(parts);
+        }
+
+
+    }
+}
