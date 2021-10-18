dc73ff282dc3074803a4b60efc469f0e3ca88675
melyman95 <mattlyman12@gmail.com>
2021-10-17 19:19:49 -0700

    updated bootstrap

--- Controllers/PartController.cs
+++ Controllers/PartController.cs
@@ -2,6 +2,9 @@
 using InternetPcPartDatabase.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace InternetPcPartDatabase.Controllers
 {
