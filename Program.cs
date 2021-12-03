5af2bbb41ddbc5a953a3f48944cc04600cf5ed22
melyman95 <mattlyman12@gmail.com>
2021-12-02 19:21:46 -0800

    added sendgrid, still troubleshooting why email is not being sent and breakpoint not being tripped

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using InternetPcPartDatabase.Data;
 using InternetPcPartDatabase.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 #nullable disable
 
@@ -24,6 +26,20 @@ builder.Services.AddSession(options =>
 });
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// SendGrid
+var apiKey = builder.Configuration.GetSection("SENDGRID_KEY").Value;
+var client = new SendGridClient(apiKey);
+var message = new SendGridMessage()
+{
+    From = new EmailAddress("gojem73112@terasd.com", "IPCDB"),
+    Subject = "Sending Email with SendGrid",
+    PlainTextContent = "It's easy to do",
+    HtmlContent = "<strong> and easy to do anywhere</strong>"
+};
+message.AddTo(new EmailAddress("mattlyman12@gmail.com", "Matt Lyman"));
+var response = await client.SendEmailAsync(message).ConfigureAwait(false);
+//
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
